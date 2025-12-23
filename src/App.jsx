8820b2b94870b80638f50a959df4647e46ff7d4f
sai chandu <saichandumucharla@gmail.com>
2025-12-23 21:10:34 +0530

    Fix student dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import StudentDashboard from "./pages/StudentDashboard";
-import MentorDashboard from "./pages/MentorDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import AdminCreateCourse from "./pages/AdminCreateCourse";
-import AdminCreateMentor from "./pages/AdminCreateMentor";
-import AdminCourseChapters from "./pages/AdminCourseChapters";
-import StudentCourseDetail from "./pages/StudentCourseDetail";
+import MentorDashboard from "./pages/MentorDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -19,19 +15,10 @@ function App() {
         <Route path="/register" element={<Register />} />
 
         <Route
-          path="/student-dashboard"
+          path="/admin-dashboard"
           element={
-            <ProtectedRoute allowedRoles={["STUDENT"]}>
-              <StudentDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/student/course/:courseId"
-          element={
-            <ProtectedRoute allowedRoles={["STUDENT"]}>
-              <StudentCourseDetail />
+            <ProtectedRoute role="ADMIN">
+              <AdminDashboard />
             </ProtectedRoute>
           }
         />
@@ -39,44 +26,17 @@ function App() {
         <Route
           path="/mentor-dashboard"
           element={
-            <ProtectedRoute allowedRoles={["MENTOR"]}>
+            <ProtectedRoute role="MENTOR">
               <MentorDashboard />
             </ProtectedRoute>
           }
         />
 
         <Route
-          path="/admin-dashboard"
+          path="/student-dashboard"
           element={
-            <ProtectedRoute allowedRoles={["ADMIN"]}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin-create-course"
-          element={
-            <ProtectedRoute allowedRoles={["ADMIN"]}>
-              <AdminCreateCourse />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin-create-mentor"
-          element={
-            <ProtectedRoute allowedRoles={["ADMIN"]}>
-              <AdminCreateMentor />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin-course/:courseId/chapters"
-          element={
-            <ProtectedRoute allowedRoles={["ADMIN"]}>
-              <AdminCourseChapters />
+            <ProtectedRoute role="STUDENT">
+              <StudentDashboard />
             </ProtectedRoute>
           }
         />
